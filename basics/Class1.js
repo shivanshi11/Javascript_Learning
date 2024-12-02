13ab6fb530ebc7e35327b2a726521de0c4955d4f
Shivanshi11 <shivanshishukla011@gmail.com>
2024-12-03 00:33:50 +0530

    class1

---
+++ basics/Class1.js
@@ -0,0 +1,27 @@
+// console.log("Shivanshi");
+
+const accoountId = 1676767;
+let accountEmail = "shivanshi@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountstate;
+// give undefined
+
+
+// var kahi bhi use ho jata hai 
+
+// const me value change nahi kar skte 
+// accoountId = 2;  not allowed 
+
+accountEmail = "hs@gmail.com"
+accountPassword = "6464564";
+accountCity="delhi"
+
+
+// prefer not to use var 
+// because of issue in block scope and functional scope
+
+console.log("Account ID: ", accoountId);
+
+console.table([accoountId,accountEmail,accountPassword,accountCity,accountstate]);
+// /give repsone in the table format
---
+++ basics/Datatype2.js
@@ -0,0 +1,32 @@
+"use Strict"; //treat all JS code as newer version
+
+// alert("hello")  //we are using nodejs not browser 
+
+console.log(3 + 3);
+console.log("honey");
+
+
+let number = 55;
+let name = "shivannshi";
+let isLoggedin = false;
+let state; //defined nahi kea 
+let state1 = null ; //aayi hi nahi hai value
+
+
+// number =>v2 to power 53
+//  bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol
+
+
+
+///////////////Object//////////////////////////
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object 
+
+
+// undefined undefined hai but null ek object => interview question
